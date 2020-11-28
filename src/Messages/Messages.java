fbfcecae9248180fabc11ebc3f754cc6394375dd
Daniel Ruiz Mejia <daniel.ruiz-mejia@etu.utc.fr>
2020-11-28 17:35:33 +0100

    Update Messages.java

--- src/Messages/Messages.java
+++ src/Messages/Messages.java
@@ -3,6 +3,7 @@ import jade.lang.acl.ACLMessage;
 
 public class Messages {
 	public static ACLMessage Subscribe(int type,String name,String content,boolean islocal) {
+	    /*type = performative, name =  destinataire, content= contenu de message,  islocal = ISLOCALNAME*/
 		ACLMessage message = new ACLMessage(type);
 		message.addReceiver(new AID(name,islocal));
 		message.setContent(content);
