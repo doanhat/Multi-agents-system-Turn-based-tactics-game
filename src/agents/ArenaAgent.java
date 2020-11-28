e4c5f45f0ab9b392b57a25b7ce73893d89b14218
Daniel Ruiz Mejia <daniel.ruiz-mejia@etu.utc.fr>
2020-11-28 17:33:04 +0100

    Update ArenaAgent.java

--- src/agents/ArenaAgent.java
+++ src/agents/ArenaAgent.java
@@ -4,11 +4,14 @@ import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class ArenaAgent extends Agent {
+    
+    MessageTemplate subscribe_template = MessageTemplate.MatchPerformative(ACLMessage.SUBSCRIBE);
 
 	public void Setup() {
 		System.out.println(getLocalName() + "--> Installed");
 		addBehaviour(new Subscribe_Agent_Matchmaking());
 		addBehaviour(new Subscribe_Agent_Classement());
+		addBehaviour(new Attribution_de_Joueurs());
 	}
 
 	public class Subscribe_Agent_Matchmaking extends OneShotBehaviour {
@@ -20,6 +23,16 @@ public class ArenaAgent extends Agent {
 	public class Subscribe_Agent_Classement extends OneShotBehaviour {
 		public void action() {
 			send(Messages.Subscribe(ACLMessage.INFORM,"RankingAgent",getLocalName(),AID.ISLOCALNAME));
+		}
+	}
+	
+	public class Attribution_de_Joueurs extends CyclicBehaviour {
+		public void action() {
+			ACLMessage message = receive(subscribe_template);
+			if (message != null) {
+				/*commencer la bataille avec les agents*/
+			} else
+				block();
 		}
 	}
 
