b746cf2d7c0fa319bc77ee2a2a8124e7b023825f
Daniel Ruiz Mejia <daniel.ruiz-mejia@etu.utc.fr>
2020-11-28 18:18:18 +0100

    Update ArenaAgent.java

--- src/agents/ArenaAgent.java
+++ src/agents/ArenaAgent.java
@@ -5,8 +5,8 @@ import jade.lang.acl.ACLMessage;
 
 public class ArenaAgent extends Agent {
     
-    MessageTemplate subscribe_template = MessageTemplate.MatchPerformative(ACLMessage.SUBSCRIBE);
-    MessageTemplate inform_template = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+    	MessageTemplate subscribe_template = MessageTemplate.MatchPerformative(ACLMessage.SUBSCRIBE);
+	MessageTemplate inform_template = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
 	public int nb_joueurs;
 	public int reponses;
 
@@ -19,16 +19,17 @@ public class ArenaAgent extends Agent {
 
 	public class Subscribe_Agent_Matchmaking extends OneShotBehaviour {
 		public void action() {
-			send(Messages.Subscribe(ACLMessage.INFORM,"MatchmakerAgent",getLocalName(),AID.ISLOCALNAME));
+			send(Messages.Subscribe(ACLMessage.INFORM, "MatchmakerAgent", getLocalName(), AID.ISLOCALNAME));
 		}
 	}
-	
+
 	public class Subscribe_Agent_Classement extends OneShotBehaviour {
 		public void action() {
-			send(Messages.Subscribe(ACLMessage.INFORM,"RankingAgent",getLocalName(),AID.ISLOCALNAME));
+			send(Messages.Subscribe(ACLMessage.INFORM, "RankingAgent", getLocalName(), AID.ISLOCALNAME));
 		}
 	}
-	
+
+	//1 )L’Agent Arène reçoit la composition des équipes par l’Agent Matchmaker
 	public class Attribution_de_Joueurs extends CyclicBehaviour {
 		public void action() {
 			ACLMessage message = receive(subscribe_template);
@@ -37,20 +38,22 @@ public class ArenaAgent extends Agent {
 				int size =  4; // le nombre de joueurs qu'il reçoit du matchmaking
 				nb_joueurs = size;
 				String[] names_Joeurs = new String[size];// le nom de chaque de joueur qu'il reçoit du matchmaking
-				for(int i = 0; i<size;i++) {
+				// 2) L’Agent Arène envoie un message aux agents joueurs
+				for(int i = 0; i<size;i++) { 
 					addBehaviour(new interaction_joueur_arene(myAgent,Messages.Subscribe(ACLMessage.REQUEST,names_Joeurs[i], getLocalName(), AID.ISLOCALNAME)));
 				}
 			} else
 				block();
 		}
 	}
-	
+	//3) Les Agents Joueurs répondent à l’agent Arène en fournissant leurs caractéristiques
 	public class interaction_joueur_arene extends AchieveREInitiator{
 		public interaction_joueur_arene(Agent a, ACLMessage msg) {
 			super(a, msg);
 		}
 		protected void handleInform(ACLMessage inform) {
 			/*sauvegarder les caractéristiques du joueur*/
+			//4) L’Agent Arène récupère toutes les données et commence le combat tour par tour
 			reponses++;
 			if(reponses==nb_joueurs) { /*si nous avons obtenu les caractéristiques de tous les joueurs et que nous pouvons commencer le combat*/
 				
